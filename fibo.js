e4457c4121bd088aded313408e3eec29cbe12fc6
Оксана <oksankagnyp@gmail.com>
2018-09-04 10:16:13 +0000

    п

---
+++ fibo.js
@@ -0,0 +1,9 @@
+let ask=parseInt(prompt("Введіть число Фібоначе"));
+
+let f=b(1.618);
+//alert(f);
+function b(num){
+return Math.pow(num,ask);}
+
+let fibo=(f/2.236);
+alert(fibo +  "     -заокруглюєм в сторону ближчу до рівного числа, це й буде ваше число Фібоначе!");
